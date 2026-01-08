21a05fa97b27e9ccdb7737abe71dee1f6d076aae
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-09 04:58:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ connectToWhatsApp()
 
 // 🌐 HTTP server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🌐 HTTP server running on port ${PORT}`);
 });
