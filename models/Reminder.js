02cc8380e0843ccc8c50fdbb353ba3e75bf2927c
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-09 17:43:34 +0530

    Update Reminder.js

--- models/Reminder.js
+++ models/Reminder.js
@@ -1,1 +1,48 @@
+const mongoose = require("mongoose");
 
+const reminderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      required: true, // WhatsApp number
+    },
+
+    type: {
+      type: String,
+      required: true,
+      enum: [
+        "water",
+        "meal",
+        "dua",
+        "week",
+        "trimester",
+        "appointment",
+      ],
+    },
+
+    scheduledAt: {
+      type: Date,
+      required: true,
+    },
+
+    // extra info (meal name, week number, image, etc.)
+    data: {
+      type: Object,
+      default: {},
+    },
+
+    sent: {
+      type: Boolean,
+      default: false,
+    },
+
+    sentAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Reminder", reminderSchema);
