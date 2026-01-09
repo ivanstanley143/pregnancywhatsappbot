903df8dd2a4f483c03f8e6a012f46b63f8568db4
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-09 18:33:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
 
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("🗄️ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed", err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
