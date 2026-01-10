a6ecff681bcdc1addf6e1b4ca06887ac50575e3e
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-10 19:09:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI missing");
-  }
-
   await mongoose.connect(process.env.MONGO_URI);
   console.log("🗄️ MongoDB connected");
 }
