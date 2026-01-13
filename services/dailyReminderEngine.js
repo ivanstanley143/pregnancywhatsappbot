176c8cc2ede037ee536b4001e413fdcdb39b1733
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-13 13:09:11 +0530

    Update dailyReminderEngine.js

--- services/dailyReminderEngine.js
+++ services/dailyReminderEngine.js
@@ -2,17 +2,33 @@ const Reminder = require("../models/Reminder");
 const { sendTemplate } = require("../whatsappCloud");
 
 async function processDailyReminders() {
+  const now = new Date();
+
   const list = await Reminder.find({
     sent: false,
-    scheduledAt: { $lte: new Date() }
+    scheduledAt: { $lte: now }
   });
 
   for (const r of list) {
-    await sendTemplate(r.user, "pregnancy_water_reminder");
-    r.sent = true;
-    await r.save();
+    try {
+      if (r.type === "water") {
+        await sendTemplate(r.user, "pregnancy_water_reminder");
+      }
+
+      if (r.type === "meal") {
+        await sendTemplate(r.user, "pregnancy_meal_reminder");
+      }
+
+      if (r.type === "dua") {
+        await sendTemplate(r.user, "pregnancy_dua");
+      }
+
+      r.sent = true;
+      await r.save();
+    } catch (err) {
+      console.error("❌ Send failed", err.response?.data || err.message);
+    }
   }
 }
 
 module.exports = { processDailyReminders };
-
