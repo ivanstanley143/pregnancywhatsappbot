609b5fefc1847e16c1d20ce9af982a5c7ff764fa
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-13 15:19:53 +0530

    Update duaEngine.js

--- services/duaEngine.js
+++ services/duaEngine.js
@@ -1,19 +1,18 @@
+const { sendTemplate } = require("../whatsappCloud");
 const data = require("../data");
-const utils = require("../utils");
-const { sendTextMessage } = require("../whatsappCloud");
-
-function getCurrentDua() {
-  const week = utils.getPregnancyWeek();
-  return data.WEEKLY_DUA[week] || { title: "🤲 Dua", text: "Allahumma ihfazna" };
-}
-
-async function sendWeeklyDua() {
-  const d = getCurrentDua();
-  await sendTextMessage(data.USER, utils.format(`${d.title}\n${d.text}`));
-}
 
 async function sendDailyDua() {
-  await sendWeeklyDua();
+  const dua = data.WEEKLY_DUA[data.CURRENT_WEEK];
+
+  await sendTemplate(data.USER, "pregnancy_dua", [
+    {
+      type: "body",
+      parameters: [
+        { type: "text", text: dua.arabic },
+        { type: "text", text: dua.english }
+      ]
+    }
+  ]);
 }
 
-module.exports = { sendWeeklyDua, sendDailyDua };
+module.exports = { sendDailyDua };
