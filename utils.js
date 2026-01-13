0080dc533a2961031977e0590e0c7429ac18ecc0
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-14 01:00:45 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,57 +1,18 @@
-const Reminder = require("./models/Reminder");
 const data = require("./data");
-const moment = require("moment-timezone");
 
-async function seedDailyReminders() {
-  await Reminder.deleteMany({ type: { $in: ["water", "meal"] } });
-
-  const tz = process.env.TIMEZONE || "Asia/Kolkata";
-
-  // WATER reminders
-  for (const t of data.WATER_TIMES) {
-    const [hour, minute] = t.split(":");
-
-    const time = moment()
-      .tz(tz)
-      .hour(parseInt(hour))
-      .minute(parseInt(minute))
-      .second(0);
-
-    if (time.isBefore(moment().tz(tz))) {
-      time.add(1, "day");
-    }
-
-    await Reminder.create({
-      type: "water",
-      scheduledAt: time.toDate()
-    });
-  }
-
-  // MEAL reminders
-  for (const timeKey of Object.keys(data.MEALS)) {
-    const [hour, minute] = timeKey.split(":");
-
-    const time = moment()
-      .tz(tz)
-      .hour(parseInt(hour))
-      .minute(parseInt(minute))
-      .second(0);
-
-    if (time.isBefore(moment().tz(tz))) {
-      time.add(1, "day");
-    }
-
-    await Reminder.create({
-      type: "meal",
-      scheduledAt: time.toDate(),
-      data: {
-        title_en: data.MEALS[timeKey][0],
-        title_ml: data.MEALS[timeKey][1]
-      }
-    });
-  }
-
-  console.log("✅ Daily water + meal reminders seeded");
+function getPregnancyWeek() {
+  const diff = (new Date() - new Date(data.LMP)) / (1000 * 60 * 60 * 24);
+  return Math.floor(diff / 7) + 1;
 }
 
-module.exports = { seedDailyReminders };
+function getTrimester(week) {
+  if (week <= 12) return 1;
+  if (week <= 27) return 2;
+  return 3;
+}
+
+function format(text) {
+  return `${text}\n\n${data.FOOTER}`;
+}
+
+module.exports = { getPregnancyWeek, getTrimester, format };
