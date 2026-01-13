c8e5bcfa678d0419ab6c68c615780a5532ed2808
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-14 00:36:54 +0530

    Delete services directory

--- services/commandRouter.js
+++
@@ -1,8 +0,0 @@
-const { handleFoodCommand } = require("./foodEngine");
-const { handleAppointmentCommand } = require("./appointmentCommands");
-
-async function routeCommand(text) {
-  return handleFoodCommand(text) || await handleAppointmentCommand(text);
-}
-
-module.exports = { routeCommand };
