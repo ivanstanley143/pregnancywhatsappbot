b9210f93ace191aba5e16f874017aa723d93d056
KEERTHY SAJI <66038840+ivanstanley143@users.noreply.github.com>
2026-01-15 13:45:32 +0530

    Create templateImages.js

---
+++ templateImages.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pregnancy_food_safe: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768460707/Gemini_Generated_Image_y8sqsdy8sqsdy8sq_1_m2p0hv.jpg",
+  pregnancy_food_limit: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768460390/Gemini_Generated_Image_7kgk987kgk987kgk_3_iwsked.jpg",
+  pregnancy_food_avoid: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768460561/Gemini_Generated_Image_jhri9xjhri9xjhri_2_rfzw9u.jpg",
+
+  pregnancy_week_12: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768464844/file_000000002f447207bffe5ab6c0f843fc_lppv36.jpg",
+  pregnancy_week_13: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768464841/file_0000000075ec72069b52c6d13eb158cd_iqtoke.jpg",
+  pregnancy_week_14_v1: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768464842/file_00000000ae207206a91304441fc049cc_wg0en4.jpg",
+  pregnancy_week_15: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768464841/file_00000000a2487206b0277bcf6ef2757e_cybdg9.jpg",
+
+  pregnancy_trimester_1: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768460904/file_00000000a9c47209958c868a7d4aaa1e_4_jwpzds.jpg",
+  pregnancy_trimester_2: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768460987/file_00000000fc48720685063f2cf441d60a_3_vf4hif.jpg",
+  pregnancy_trimester_3: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768461059/file_0000000013c0720681789ce45f4f039f_3_qxyb8c.jpg",
+
+  pregnancy_appointment: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768458994/Screenshot_2026-01-12_135725_ajrs86.png",
+  pregnancy_meal_reminder: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768458867/oardefault_1_agxi79.jpg",
+  pregnancy_water_reminder_v1: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768458692/Gemini_Generated_Image_pbueuhpbueuhpbue_unf6s0.jpg",
+
+  pregnancy_dua: "https://res.cloudinary.com/drcqtmobe/image/upload/v1768457852/517965945_10227891801024733_1699566531759542400_n_1_ttdeai.jpg"
+};
